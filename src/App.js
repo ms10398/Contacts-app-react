bf71ad0266ac1b9b201ec401fad1fe481c427e35
Mohit Sharma <msvbs98@gmail.com>
2018-02-23 20:44:26 +0530

    Working Contacts app
    Woahh nice one!

--- src/App.js
+++ src/App.js
@@ -23,10 +23,16 @@ class App extends Component {
     }))
 
     ContactsAPI.remove(contact)
-
     // setState second method
     // this.setState({ Object with the state })
+  }
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }))
+    })
   }
 
   render() {
@@ -37,7 +43,14 @@ class App extends Component {
             contacts={this.state.contacts}
           />
       )}/>
-      <Route path="/create" component={CreateContact}/>
+    <Route path="/create" render={({ history }) => (
+        <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+        />
+    )}/>
     </div>
     )
   }
